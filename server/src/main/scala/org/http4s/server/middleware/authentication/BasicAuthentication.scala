c5085fceaafd6fda3a87d14b4aadf186955b0463
Felipe Kurkowski <felipekurkowski@gmail.com>
2015-11-15 17:31:33 -0200

    Minor refactoring on basic authentication.

--- server/src/main/scala/org/http4s/server/middleware/authentication/BasicAuthentication.scala
+++ server/src/main/scala/org/http4s/server/middleware/authentication/BasicAuthentication.scala
@@ -4,7 +4,7 @@ package middleware
 package authentication
 
 import org.http4s.headers.Authorization
-import scalaz._
+import scalaz._, Scalaz._
 import scalaz.concurrent.Task
 
 /**
@@ -15,28 +15,20 @@ import scalaz.concurrent.Task
  */
 class BasicAuthentication(realm: String, store: AuthenticationStore) extends Authentication {
 
-  private trait AuthReply
-  private case class OK(user: String, realm: String) extends AuthReply
-  private case object NeedsAuth extends AuthReply
-
   protected def getChallenge(req: Request) = checkAuth(req).map {
-    case OK(user, realm) => \/-(addUserRealmAttributes(req, user, realm))
-    case NeedsAuth       => -\/(Challenge("Basic", realm, Nil.toMap))
+    case Some(user) => \/-(addUserRealmAttributes(req, user, realm))
+    case None       => -\/(Challenge("Basic", realm, Map.empty))
   }
 
-  private def checkAuth(req: Request): Task[AuthReply] = {
-    req.headers.get(Authorization) match {
-      case Some(Authorization(BasicCredentials(user, client_pass))) =>
-        store(realm, user).map {
-          case None => NeedsAuth
-          case Some(server_pass) =>
-            if (server_pass == client_pass) OK(user, realm)
-            else NeedsAuth
-        }
-
-      case Some(Authorization(_)) => Task.now(NeedsAuth)
-
-      case None => Task.now(NeedsAuth)
-    }
+  private def checkAuth(req: Request): Task[Option[String]] = {
+    req.headers.get(Authorization)
+      .filter(_.credentials.authScheme == AuthScheme.Basic)
+      .traverseM {
+        case Authorization(BasicCredentials(user, password)) =>
+          store(realm, user) map {
+            case Some(srvPassword) if password == srvPassword => user.some
+            case _ => none
+          }
+      }
   }
 }
